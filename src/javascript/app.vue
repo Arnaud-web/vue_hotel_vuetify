67bbf42fb7f1e1ad60a53f35f456275f709a6f20
Arnaud <mrandriamihaja2@gmail.com>
2020-11-16 20:24:14 +0300

    version stable

--- src/javascript/app.vue
+++ src/javascript/app.vue
@@ -1,130 +1,162 @@
 <template>
-  <v-app id="app" class="body_h">
+  <v-app id="app">
     <v-card class="overflow-hidden">
-    <v-app-bar
+      <v-app-bar
+        color="#fcb69f"
+        dark
+        shrink-on-scroll
+        src="https://picsum.photos/1920/1080?random"
+        scroll-target="#scrolling-techniques-2"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
 
-      color="#fcb69f"
-      dark
-      shrink-on-scroll
-      src="https://picsum.photos/1920/1080?random"
-      scroll-target="#scrolling-techniques-2"
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title> {{ message }}</v-toolbar-title>
 
-      <v-toolbar-title> {{ message }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="info" @click="(form = false), (showH = true)"
+          >Listes Hotel</v-btn
+        >
+        <v-btn
+          v-show="showLogin()"
+          color="success"
+          @click="
+            (showC = !showC), (showFormLogin = !showFormLogin), (form = !form)
+          "
+          >Compte</v-btn
+        >
+        <v-btn v-show="!showLogin()" color="success" @click="deconnect()"
+          >Déconnecter</v-btn
+        >
+        <v-text-field
+          v-show="showSearch"
+          dark
+          prepend-icon="mdi-map-search"
+          type="text"
+          placeholder="Search"
+          v-model="search"
+          @change="hotelsFilter"
+        />
+        <span v-if="search != ''" class="resulta">
+          {{ hotelsFilter().length }} resultats{{
+            hotelsFilter().length > 1 ? "s" : ""
+          }}
+        </span>
 
-      <v-spacer></v-spacer>
-      <v-btn v-show="showLogin()" color="success" @click="showC = !showC,showFormLogin = !showFormLogin">{{
-        !showC ? "Cree un Compte" : "se Connecté"
-      }}</v-btn>
-        <v-btn v-show="!showLogin()" color="success" @click="deconnect()">Déconnecter</v-btn>
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+        <v-btn @click="showSearch = !showSearch" icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-
-    </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-2"
-      class="overflow-y-auto"
-      max-height="1000"
-    >
-      <v-container style="height: 100%;">
-
-        <div v-show="showH">
-
-          <div v-show="showLogin()">
-            <Segnup v-show="showC" />
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-sheet
+        id="scrolling-techniques-2"
+        class="overflow-y-auto"
+        max-height="100%"
+      >
+        <v-container style="height: 550px">
+          <div v-show="form">
+            <div v-show="showLogin()">
+              <v-btn
+                v-show="showLogin()"
+                color="success"
+                @click="(showC = !showC), (showFormLogin = !showFormLogin)"
+                >{{ !showC ? "Cree un Compte" : "se Connecté" }}</v-btn
+              >
+              <Segnup v-show="showC" />
+            </div>
+            <LoginApp v-show="!showC" v-if="showLogin()" />
+            <!-- {{ this.$store.state.user }} -->
+            <div class="create_" v-if="!showLogin()">
+              <Create v-if="this.$store.state.user.role == 'hotel'" />
+            </div>
           </div>
-          <LoginApp v-show="!showC" v-if="showLogin()" />
-          <!-- {{ this.$store.state.user }} -->
-          <div class="create_" v-if="!showLogin()">
-            <Create v-if="this.$store.state.user.role == 'hotel'" />
-          </div>
-          <v-container width="400px">
-            <v-card-text>
-              <v-text-field
-                prepend-icon="mdi-map-search"
-                type="text"
-                placeholder="Search"
-                v-model="search"
-                @change="hotelsFilter"
-              />
-              <span v-if="search != ''" class="resulta">
-                {{ hotelsFilter().length }} resultats{{
-                  hotelsFilter().length > 1 ? "s" : ""
-                }}
-              </span>
-            </v-card-text>
-          </v-container>
-          <div
-            class="list_hotel"
-            v-for="(hotel, index) in hotelsFilter()"
-            :key="index"
-          >
-            <div style="display: inline-flex">
-              <div>
-                <Hotel :hotel="hotel" />
-                <div class="btn_options">
-                  <!-- <v-btn
+
+          <div v-if="!form">
+            <div v-show="showH">
+              <v-container width="400px">
+                <v-card-text>
+                  <v-text-field
+                    prepend-icon="mdi-map-search"
+                    type="text"
+                    placeholder="Search"
+                    v-model="search"
+                    @change="hotelsFilter"
+                  />
+                  <span v-if="search != ''" class="resulta">
+                    {{ hotelsFilter().length }} resultats{{
+                      hotelsFilter().length > 1 ? "s" : ""
+                    }}
+                  </span>
+                </v-card-text>
+              </v-container>
+              <div
+                class="list_hotel"
+                v-for="(hotel, index) in hotelsFilter()"
+                :key="index"
+              >
+                <div style="display: inline-flex">
+                  <div>
+                    <Hotel :hotel="hotel" />
+                    <div class="btn_options">
+                      <!-- <v-btn
                     v-if="!showLogin()"
                     class="btn btn-sm btn-info"
                     @click="showFormEmail = !showFormEmail"
                   >
                     Reservé
                   </v-btn> -->
-                  <v-btn color="green" @click="show(hotel)"> show </v-btn>
-                  <!-- <a class="btn btn" @click="addToCart(hotel)">
+                      <v-btn color="green" @click="show(hotel)"> show </v-btn>
+                      <!-- <a class="btn btn" @click="addToCart(hotel)">
               reserve
               </a> -->
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div v-if="showH == false">
-          <ShowHotel :hotel="hotel" />
-          <div class="btn_options">
-            <!-- <a class="btn btn" @click="addToCart(hotel)">
+
+          <div v-if="showH == false">
+            <ShowHotel :hotel="hotel" />
+            <div class="btn_options">
+              <!-- <a class="btn btn" @click="addToCart(hotel)">
               reserve
               </a> -->
-            <v-btn
-              v-if="!showLogin()"
-              color="green"
-              @click="showFormEmail = !showFormEmail"
-            >
-              Reservé
-            </v-btn>
-            <v-btn color="info" @click="show(hotel)"> Listes Hotels </v-btn>
+              <v-btn
+                v-if="!showLogin()"
+                color="green"
+                @click="showFormEmail = !showFormEmail"
+              >
+                Reservé
+              </v-btn>
+              <v-btn color="info" @click="show(hotel)"> Listes Hotels </v-btn>
+            </div>
+            <div v-if="showFormEmail">
+              <FormMail :hotel="hotel" />
+            </div>
+            <div>
+              <CommentShow :hotel="hotel" />
+            </div>
+            <div v-if="!showLogin()">
+              <Comments :hotel="hotel" />
+            </div>
           </div>
-          <div v-if="showFormEmail">
-            <FormMail :hotel="hotel" />
-          </div>
-          <div>
-            <CommentShow :hotel="hotel" />
-          </div>
-          <div v-if="!showLogin()">
-            <Comments :hotel="hotel" />
-          </div>
-        </div>
-      </v-container>
-    </v-sheet>
-  </v-card>
-
+        </v-container>
+      </v-sheet>
+    </v-card>
   </v-app>
 </template>
 
@@ -148,7 +180,9 @@ export default {
       showC: false,
       hotel: "",
       showFormEmail: false,
-      showFormLogin:false,
+      showFormLogin: false,
+      showSearch: false,
+      form: false,
     };
   },
   components: {
@@ -263,7 +297,8 @@ p {
 .body_h {
   margin: 20px;
   text-align: center;
-}.create_{
+}
+.create_ {
   margin: 2px;
   padding: 4px;
   display: block;
